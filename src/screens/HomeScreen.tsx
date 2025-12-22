a3fbc8f2fceccfb825024b89a98921a8c9c45873
yasminghassani <yasminghassani@gmail.com>
2025-12-22 14:18:17 +0800

    update HomeScreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, ActivityIndicator } from 'react-native';
 import { ScrollView } from 'react-native';
 import CustomCard from '../components/CustomCard';
 
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+};
+
 const HomeScreen: React.FC = () => {
+  const [count, setCount] = useState(0);
+
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => {
+        setUsers(data);
+        setLoading(false);
+      });
+  }, []); // Runs only once on mount
+
+  if (loading) return <ActivityIndicator size="large" />;
+
   return (
     <ScrollView>
       <CustomCard
@@ -12,6 +36,15 @@ const HomeScreen: React.FC = () => {
         buttonText="Press Me"
         onPress={() => console.log('Home Card Pressed')}
       />
+      <View style={{ padding: 20, display: 'none' }}>
+        <Text>Count: {count}</Text>
+        <Button title="Increase" onPress={() => setCount(count + 1)} />
+      </View>
+      <View style={{ padding: 20, display: 'none' }}>
+        {users.map(user => (
+          <Text key={user?.id}>{user?.name}</Text>
+        ))}
+      </View>
     </ScrollView>
   );
 };
