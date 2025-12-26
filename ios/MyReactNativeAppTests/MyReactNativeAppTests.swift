6d893853fcaea5a411d8cbf455fcc29b9fec6730
yasminghassani <yasminghassani@gmail.com>
2025-12-26 10:41:40 +0800

    implement fastlane

---
+++ ios/MyReactNativeAppTests/MyReactNativeAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  MyReactNativeAppTests.swift
+//  MyReactNativeAppTests
+//
+//  Created by YASMIN on 26/12/25.
+//
+
+import Testing
+
+struct MyReactNativeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
